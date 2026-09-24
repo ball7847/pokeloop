060888c03fafbd048006d81ada3cc9d842a82a5b
ball7847 <ball7847@gmail.com>
2026-09-24 23:39:51 +0900

    Split game data into data/index.js

---
+++ data/index.js
@@ -0,0 +1,11 @@
+window.POKELOOP_DATA_PARTS = window.POKELOOP_DATA_PARTS || {};
+
+window.POKELOOP_DATA = {
+  pokemon: window.POKELOOP_DATA_PARTS.pokemon || [],
+  moves: window.POKELOOP_DATA_PARTS.moves || [],
+  items: window.POKELOOP_DATA_PARTS.items || [],
+  recipes: window.POKELOOP_DATA_PARTS.recipes || [],
+  merchantStock: window.POKELOOP_DATA_PARTS.merchantStock || [],
+  regions: window.POKELOOP_DATA_PARTS.regions || [],
+  depths: window.POKELOOP_DATA_PARTS.depths || []
+};
