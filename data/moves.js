89c9affd9695c6548afb109e06dc5661a9f93503
ball7847 <ball7847@gmail.com>
2026-09-24 23:39:38 +0900

    Split game data into data/moves.js

---
+++ data/moves.js
@@ -0,0 +1,13 @@
+window.POKELOOP_DATA_PARTS = window.POKELOOP_DATA_PARTS || {};
+window.POKELOOP_DATA_PARTS.moves = [
+  { id:"AS001", name:"덩굴채찍", kind:"속공", type:"풀", power:10, ppGain:20, description:"덩굴을 휘둘러 공격하고 PP를 획득한다." },
+  { id:"SS001", name:"씨폭탄", kind:"강공", type:"풀", power:42, ppCost:100, description:"커다란 씨앗을 폭발시켜 강한 피해를 준다." },
+  { id:"AS002", name:"불꽃세례", kind:"속공", type:"불꽃", power:11, ppGain:20, description:"작은 불꽃으로 공격하고 PP를 획득한다." },
+  { id:"SS002", name:"화염바퀴", kind:"강공", type:"불꽃", power:46, ppCost:100, description:"불꽃을 두르고 강하게 돌진한다." },
+  { id:"AS003", name:"물대포", kind:"속공", type:"물", power:9, ppGain:25, description:"물을 발사해 공격하고 PP를 획득한다." },
+  { id:"SS003", name:"아쿠아테일", kind:"강공", type:"물", power:40, ppCost:100, description:"물의 힘을 두른 꼬리로 강하게 공격한다." },
+  { id:"AS004", name:"전광석화", kind:"속공", type:"노말", power:10, ppGain:25, description:"빠르게 접근해 공격한다." },
+  { id:"SS004", name:"필살앞니", kind:"강공", type:"노말", power:38, ppCost:100, description:"강한 앞니로 상대를 물어뜯는다." },
+  { id:"AS005", name:"바람일으키기", kind:"속공", type:"비행", power:10, ppGain:20, description:"날개로 바람을 일으켜 공격한다." },
+  { id:"SS005", name:"날개치기", kind:"강공", type:"비행", power:40, ppCost:100, description:"날개를 크게 휘둘러 공격한다." }
+];
