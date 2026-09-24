18e4e7480f33777489365d71bb6a7a2dbb3a7b36
ball7847 <ball7847@gmail.com>
2026-09-24 22:51:56 +0900

    Expand owned Pokemon data model for detailed profile UI

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 window.POKELOOP_DATA = {
   pokemon: [
-    { id:"001", name:"이상해씨", type:["풀","독"], level:5, hp:72, atk:15, def:12, quick:"덩굴채찍", strong:"씨폭탄", item:null, icon:"001" },
-    { id:"004", name:"파이리", type:["불꽃"], level:5, hp:64, atk:18, def:9, quick:"불꽃세례", strong:"화염바퀴", item:null, icon:"004" },
-    { id:"007", name:"꼬부기", type:["물"], level:5, hp:80, atk:13, def:16, quick:"물대포", strong:"아쿠아테일", item:null, icon:"007" },
-    { id:"019", name:"꼬렛", type:["노말"], level:4, hp:58, atk:16, def:8, quick:"전광석화", strong:"필살앞니", item:null, icon:"019" },
-    { id:"016", name:"구구", type:["노말","비행"], level:4, hp:60, atk:14, def:9, quick:"바람일으키기", strong:"날개치기", item:null, icon:"016" }
+    { id:"001", name:"이상해씨", nickname:"", tier:1, nature:"차분", gender:"수컷", level:5, exp:42, expRequired:100, type:["풀","독"], hp:72, atk:15, def:12, spatk:18, spdef:18, speed:13, abilities:["심록Ⅰ"], item:null, quick:"덩굴채찍", strong1:"씨폭탄", strong2:null, icon:"001" },
+    { id:"004", name:"파이리", nickname:"", tier:1, nature:"고집", gender:"수컷", level:5, exp:36, expRequired:100, type:["불꽃"], hp:64, atk:18, def:9, spatk:17, spdef:11, speed:18, abilities:["맹화Ⅰ"], item:null, quick:"불꽃세례", strong1:"화염바퀴", strong2:null, icon:"004" },
+    { id:"007", name:"꼬부기", nickname:"", tier:1, nature:"대담", gender:"수컷", level:5, exp:58, expRequired:100, type:["물"], hp:80, atk:13, def:16, spatk:14, spdef:18, speed:10, abilities:["급류Ⅰ"], item:null, quick:"물대포", strong1:"아쿠아테일", strong2:null, icon:"007" },
+    { id:"019", name:"꼬렛", nickname:"", tier:1, nature:"명랑", gender:"암컷", level:4, exp:24, expRequired:80, type:["노말"], hp:58, atk:16, def:8, spatk:7, spdef:9, speed:19, abilities:["몸통박치기"], item:null, quick:"전광석화", strong1:"필살앞니", strong2:null, icon:"019" },
+    { id:"016", name:"구구", nickname:"", tier:1, nature:"성급", gender:"수컷", level:4, exp:31, expRequired:80, type:["노말","비행"], hp:60, atk:14, def:9, spatk:11, spdef:10, speed:16, abilities:["쪼기"], item:null, quick:"바람일으키기", strong1:"날개치기", strong2:null, icon:"016" }
   ],
 
   moves: [
