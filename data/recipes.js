9fe4c7523cf4257f71a4ebbf6857b86b4f57a9a8
ball7847 <ball7847@gmail.com>
2026-09-24 23:39:42 +0900

    Split game data into data/recipes.js

---
+++ data/recipes.js
@@ -0,0 +1,6 @@
+window.POKELOOP_DATA_PARTS = window.POKELOOP_DATA_PARTS || {};
+window.POKELOOP_DATA_PARTS.recipes = [
+  { id:"rope", name:"튼튼한 끈", result:{ item:"fiber", amount:1 }, ingredients:[{item:"fiber",amount:2}], seconds:15, description:"간단한 제작 재료." },
+  { id:"rockyHelmet", name:"울퉁불퉁멧", result:{ item:"rockyHelmet", amount:1 }, ingredients:[{item:"stone",amount:6},{item:"fiber",amount:4}], seconds:60, description:"접촉 공격을 되돌려주는 도구." },
+  { id:"clearAmulet", name:"클리어참", result:{ item:"clearAmulet", amount:1 }, ingredients:[{item:"stone",amount:4},{item:"wood",amount:4}], seconds:90, description:"능력치 하락을 막는 도구." }
+];
