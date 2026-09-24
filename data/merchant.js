201b713ebcbc776ac819d58af12d2216f9935a17
ball7847 <ball7847@gmail.com>
2026-09-24 23:39:44 +0900

    Split game data into data/merchant.js

---
+++ data/merchant.js
@@ -0,0 +1,8 @@
+window.POKELOOP_DATA_PARTS = window.POKELOOP_DATA_PARTS || {};
+window.POKELOOP_DATA_PARTS.merchantStock = [
+  { item:"berry_green", buy:18, sell:7 },
+  { item:"berry_red", buy:18, sell:7 },
+  { item:"berry_blue", buy:18, sell:7 },
+  { item:"fiber", buy:14, sell:6 },
+  { item:"rockyHelmet", buy:260, sell:110 }
+];
