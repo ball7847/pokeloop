27cfffd509ae0535d58f5947621815df926868e1
ball7847 <ball7847@gmail.com>
2026-09-25 11:39:09 +0900

    Expose Pokemon image mappings in game data

--- data/index.js
+++ data/index.js
@@ -7,5 +7,6 @@ window.POKELOOP_DATA = {
   recipes: window.POKELOOP_DATA_PARTS.recipes || [],
   merchantStock: window.POKELOOP_DATA_PARTS.merchantStock || [],
   regions: window.POKELOOP_DATA_PARTS.regions || [],
-  depths: window.POKELOOP_DATA_PARTS.depths || []
+  depths: window.POKELOOP_DATA_PARTS.depths || [],
+  pokemonImages: window.POKELOOP_DATA_PARTS.pokemonImages || { basePath:"", entries:{} }
 };
