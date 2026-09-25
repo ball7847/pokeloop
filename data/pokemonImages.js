203642f361cbc2cbfbd3f93ab8415110dc585bf9
ball7847 <ball7847@gmail.com>
2026-09-25 11:38:22 +0900

    Add Pokemon image mapping layer

---
+++ data/pokemonImages.js
@@ -0,0 +1,12 @@
+window.POKELOOP_DATA_PARTS = window.POKELOOP_DATA_PARTS || {};
+
+window.POKELOOP_DATA_PARTS.pokemonImages = {
+  basePath: "./assets/images/pokemon/front/",
+  entries: {
+    "001": { base: "BULBASAUR.png" },
+    "004": { base: "CHARMANDER.png" },
+    "007": { base: "SQUIRTLE.png" },
+    "016": { base: "PIDGEY.png" },
+    "019": { base: "RATTATA.png", female: "RATTATA_female.png" }
+  }
+};
